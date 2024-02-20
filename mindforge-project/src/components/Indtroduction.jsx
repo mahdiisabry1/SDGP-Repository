eb2bbf1ddfa3f6ae4d94368299646297dde3e037
mahdiisabry1 <mahdiisabry@gmail.com>
2024-02-20 15:15:22 +0530

    home and blog

--- mindforge-project/src/components/Indtroduction.jsx
+++ mindforge-project/src/components/Indtroduction.jsx
@@ -6,9 +6,15 @@ const Indtroduction = () => {
 
   return (
     <div className='outerLayer'>
-        <div className='text-white text-center'>
+        <div className='innerLayer'>
             <h1 className='TheBanner'><marquee behavior="scroll" direction="right" scrollamount="30">Welcome to MindForge</marquee></h1>
             <p className='paragrapgh'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto molestiae unde inventore expedita a ipsum! Fuga dicta dolore, tempora molestias at ipsam placeat praesentium dignissimos consectetur neque eveniet? Quisquam, error praesentium? A consequuntur repellat molestias non dolorem aliquam rem voluptates? Aut magni, blanditiis perferendis impedit iusto provident perspiciatis incidunt?</p>
+            <div>
+              <button className='learn-more'><a href="/"> Explore </a></button>
+            </div>
+            <div>
+              <button className='learn-more'><a href="/"> Get Started </a></button>
+            </div>
         </div>
     </div>
   )
--- mindforge-project/src/pages/Blogs.jsx
+++ mindforge-project/src/pages/Blogs.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div></div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+        <h2>The blogs page</h2>
+        <p>Explore, Inspire, Thrive: Navigating Lifes Journey One Blog at a Time.</p>
+      </div>
+    </div>
   )
 }
+
 
 export default Blogs
