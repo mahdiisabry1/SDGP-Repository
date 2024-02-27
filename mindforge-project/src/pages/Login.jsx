fecaf286b83140b50cccc6e8451b6bbaaaf9bf58
mahdiisabry1 <mahdiisabry@gmail.com>
2024-02-27 18:50:38 +0530

    Revert

--- mindforge-project/src/pages/Login.jsx
+++ mindforge-project/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import './Login.css';
+import '../components/CSS/Login.css';
 
 const Login = ({isOpen, onClose}) => {
   const [username, setUsername] = useState('');
