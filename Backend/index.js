ad7d8c72203b230d9af99948555483096de65499
mahdiisabry1 <mahdiisabry@gmail.com>
2024-02-29 16:59:07 +0530

    UserAuth

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv')
+const authRoute = require('./Routes/auth')
 
 
 // database
@@ -15,6 +16,8 @@ const connectDB = async() => {
 }
 
 dotenv.config()
+app.use(express.json())
+app.use("/api/auth", authRoute)
 
 
 app.listen(process.env.PORT, () =>{
