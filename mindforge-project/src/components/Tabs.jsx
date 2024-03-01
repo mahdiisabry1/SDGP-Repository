3123302273f206b632bf8739e6e7abba84f189cc
Senuth De Silva <senuthdesilva37@gmail.com>
2024-03-01 15:06:35 +0530

    make a discription to tabs

--- mindforge-project/src/components/Tabs.jsx
+++ mindforge-project/src/components/Tabs.jsx
@@ -17,23 +17,23 @@ const toggleTab=(index)=>{
             <button className={toogleState ===
             1? 'tabs active-tabs' : 'tabs'}
              onClick={()=> toggleTab(1)}>
-             React
+             Front-End
              </button>
             
              <button className={toogleState ===
             2? 'tabs active-tabs' : 'tabs'}
              onClick={()=> toggleTab(2)}>
-             Angular
+             Back-End
              </button>
              <button className={toogleState ===
             3? 'tabs active-tabs' : 'tabs'}
              onClick={()=> toggleTab(3)}>
-             Front-end
+             Angular
              </button>
              <button className={toogleState ===
             4? 'tabs active-tabs' : 'tabs'}
              onClick={()=> toggleTab(4)}>
-             Back-end
+             React
              </button>
              <button className={toogleState ===
             5? 'tabs active-tabs' : 'tabs'}
@@ -48,41 +48,45 @@ const toggleTab=(index)=>{
 
                     <h2>Front-end</h2>
                     <hr />
-                    <p><li>dont delete this</li></p>
-                    <p><li>........</li></p>
-                    <p><li>Suiiii</li></p>
+                    <p><li>
+Front-end development involves creating the user interface and experience of a website or application. It encompasses coding languages such as HTML, CSS, and JavaScript, shaping how content is presented and interacted with by users. Front-end developers focus on optimizing design, accessibility, and responsiveness for seamless user navigation and engagement</li></p>
+                    <p><li>Dont Delete this (\/)
+(• ‿•)
+/👍👍</li></p>
+                    
                 </div>
                 <div className={toogleState === 2 ?
                 'content active-content':'content'}>
                     <h2>Back-end</h2>
                     <hr />
-                    <p><li>gvuvuu</li></p>
-                    <p><li>h</li></p>
-                    <p><li>Suiiii</li></p>
+                    
+                    <p><li>Backend refers to the unseen infrastructure of a software application, managing data and functionality behind the scenes. It comprises servers, databases, and logic ensuring smooth operation. Developers code in languages like Python, Java, or Node.js, handling server-side processes, database interactions, and APIs, crucial for delivering dynamic and interactive web experiences</li></p>
+                    <p><li>click here</li></p>
                 </div>
                 <div className={toogleState === 3 ?
                 'content active-content':'content'}>
                     <h2>Angular</h2>
                     <hr />
-                    <p><li>gvuvuu</li></p>
-                    <p><li>h</li></p>
-                    <p><li>Suiiii</li></p>
+                    <p><li>Angular is a popular open-source web application framework maintained by Google. It allows developers to build dynamic single-page applications (SPAs) efficiently. Using TypeScript, Angular provides a structured approach to development, emphasizing modularity and reusability of components. Its powerful features include two-way data binding, dependency injection, and robust routing capabilities</li></p>
                 </div>
                 <div className={toogleState === 4 ?
                 'content active-content':'content'}>
                     <h2>React</h2>
                     <hr />
-                    <p><li>gvuvuu</li></p>
-                    <p><li>h</li></p>
-                    <p><li>Suiiii</li></p>
+                    <p><li>React is a JavaScript library widely used for building user interfaces. Developed by Facebook, it allows developers to create reusable UI components, making the development process more efficient and manageable. With its virtual DOM, React optimizes rendering performance, providing a seamless and responsive user experience for web applications.
+
+
+
+
+</li></p>
+                    
                 </div>
                 <div className={toogleState === 5 ?
                 'content active-content':'content'}>
                     <h2>Full-Stack</h2>
                     <hr />
-                    <p><li>gvuvuu</li></p>
-                    <p><li>h</li></p>
-                    <p><li>Suiiii</li></p>
+                    <p><li>Fullstack refers to proficiency in both frontend and backend development, covering the entire spectrum of web development. A fullstack developer possesses skills in programming languages, databases, server management, and client-side frameworks. This expertise enables them to create dynamic, interactive, and functional web applications from conception to deployment, ensuring seamless user experiences</li></p>
+                    
                 </div>
             </div>
            </div>
