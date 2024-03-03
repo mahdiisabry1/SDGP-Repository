92576467d80864433dfe54a261b09681425bb0c1
00Dhananjaya <159724763+DDhananjaya01@users.noreply.github.com>
2024-03-04 03:43:15 +0530

    add background image and other changes

--- mindforge-project/src/pages/Cetification.jsx
+++ mindforge-project/src/pages/Cetification.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Tabs from '../components/Tabs'
+import React from 'react';
+import Tabs from '../components/Tabs';
 
-const Cetification = () => {
+const Certification = () => {
+  const backgroundStyles = {
+    backgroundImage: 'url("https://news.uchicago.edu/sites/default/files/styles/explainer_hero/public/images/2022-10/sgr%20A%2A%20ESO%20and%20M.%20Kornmesser%20690.jpg?h=06d036b4&itok=Lr5t57tH")', // Replace with the URL of your image
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
+
   return (
-    <div className='py-40 bg-black text-white'>
-      
-      <Tabs></Tabs>
+    <div className='py-40 text-white' style={backgroundStyles}>
+      <Tabs />
     </div>
-    
-  )
+  );
 }
 
-export default Cetification
+export default Certification;
