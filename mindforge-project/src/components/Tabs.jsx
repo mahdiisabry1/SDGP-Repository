f71fa20b25df97e3185f01451851283543c6e007
00Dhananjaya <159724763+DDhananjaya01@users.noreply.github.com>
2024-03-05 23:16:16 +0530

    modify buttons

--- mindforge-project/src/components/Tabs.jsx
+++ mindforge-project/src/components/Tabs.jsx
@@ -3,12 +3,30 @@ import './CSS/tabs.css'; // Import the CSS file
 
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState(1);
-
+  
   const handleTabClick = (tabNumber) => {
     setActiveTab(tabNumber);
   };
 
   const tabNames = ["React", "Angular", "FrontEnd", "BackEnd", "FullStack"];
+  const tabTransparency = 0.5;
+
+  function hexToRgb(hex) {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
+  }
+
+  
+  // Define a mapping of colors for each tab
+  const tabColors = {
+    1: '#4caf50', 
+    2: '#2196f3', 
+    3: '#ff9800', 
+    4: '#e91e63', 
+    5: '#795548', 
+  };
 
   return (
     <div>
@@ -17,6 +35,9 @@ const Tabs = () => {
           <div
             key={index + 1}
             className={`tab ${activeTab === index + 1 ? 'active' : ''}`}
+            style={{ 
+              backgroundColor: `rgba(${hexToRgb(tabColors[index + 1])}, ${tabTransparency})` 
+            }}
             onClick={() => handleTabClick(index + 1)}
           >
             {tabName} .
