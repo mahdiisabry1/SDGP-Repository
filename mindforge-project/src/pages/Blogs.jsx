24e2ec4dc95fdc60c2e7423bfb0a07cba5efb2cd
mahdiisabry1 <mahdiisabry@gmail.com>
2024-03-09 19:33:59 +0530

    fetching posts from the database

--- mindforge-project/src/pages/Blogs.jsx
+++ mindforge-project/src/pages/Blogs.jsx
@@ -1,9 +1,30 @@
 import { Link } from "react-router-dom";
 import BlogPosts from "../components/BlogPosts";
 import '../components/CSS/Blogs.css'
+import axios from "axios";
+import { IF, URL } from "../url";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext";
 
 
 const Blogs = () => {
+
+  const { user } = useContext(UserContext);
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = async()=>{
+    try {
+      const res = await axios.get(URL+"/api/posts/")
+      setPosts(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchPosts()
+  }, [])
+
   return (
     <div>
       <div className="py-40 bg-black text-center text-white px-4 outerLayer-blog">
@@ -15,7 +36,14 @@ const Blogs = () => {
 
       {/* The Blog Container */}
       <div className="max-w-7xl mx-auto">
-        <BlogPosts />
+        {posts.map((post) => (
+          <>
+            <Link to={user ? `/posts/post/${post._id}` : "/login"}>
+              <BlogPosts key={post._id} post={post} />
+            </Link>
+          </>
+        ))}
+        <h3 className="text-center font-bold mt-16">No posts available</h3>
       </div>
     </div>
   );
