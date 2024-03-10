07f34cb8423810658728a5a6f4036160cace2792
Senuth De Silva <senuthdesilva37@gmail.com>
2024-03-10 11:51:29 +0530

    add some files

---
+++ mindforge-project/src/components/ReactCertification.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CertificateImage from './CertificateImage'; // Path to your CertificateImage component
+
+const YourComponent = () => {
+  return (
+    <div>
+      <h1>React Certification</h1>
+      <CertificateImage imagePath="image_react.jpg" altText="React Certification" />
+    </div>
+  );
+};
+
+export default YourComponent;
