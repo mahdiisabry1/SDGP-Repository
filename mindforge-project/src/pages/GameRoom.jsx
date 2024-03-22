c84c05ecea92e0ccf079c74b9320997344169b9c
mahdiisabry1 <mahdiisabry@gmail.com>
2024-03-22 09:44:24 +0530

    Adding navbar

--- mindforge-project/src/pages/GameRoom.jsx
+++ mindforge-project/src/pages/GameRoom.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './CSS/Gameroom.css';
-import XO from './XO.jsx';
-import Candycrush from './Candycrush.jsx';
+import React, { useState } from "react";
+import "./CSS/Gameroom.css";
+import XO from "./XO.jsx";
+import Candycrush from "./Candycrush.jsx";
 import NavBar from "../components/NavBar";
 
 const GameRoom = () => {
-  const [activeTab, setActiveTab] = useState('tab1'); // State to manage active tab
+  const [activeTab, setActiveTab] = useState("tab1"); // State to manage active tab
 
   // Function to handle tab click
   const handleTabClick = (tab) => {
@@ -13,50 +13,211 @@ const GameRoom = () => {
   };
 
   return (
-    <div>
-      {/* Tab buttons */}
-      <div className="gamestab-buttons">
-        <button className={activeTab === 'tab1' ? 'active' : ''} onClick={() => handleTabClick('tab1')}>XO Game</button>
-        <button className={activeTab === 'tab2' ? 'active' : ''} onClick={() => handleTabClick('tab2')}>Candy Crush</button>
-        <button className={activeTab === 'tab3' ? 'active' : ''} onClick={() => handleTabClick('tab3')}>MoreGames</button>
+    <>
+    <NavBar/>
+      <div>
+        {/* Tab buttons */}
+        <div className="gamestab-buttons">
+          <button
+            className={activeTab === "tab1" ? "active" : ""}
+            onClick={() => handleTabClick("tab1")}
+          >
+            XO Game
+          </button>
+          <button
+            className={activeTab === "tab2" ? "active" : ""}
+            onClick={() => handleTabClick("tab2")}
+          >
+            Candy Crush
+          </button>
+          <button
+            className={activeTab === "tab3" ? "active" : ""}
+            onClick={() => handleTabClick("tab3")}
+          >
+            MoreGames
+          </button>
+        </div>
+
+        {/* Content for tabs */}
+        {activeTab === "tab1" && (
+          <div>
+            <h1 style={{ textAlign: "center" }}>TicTacToe</h1>
+            <XO />
+          </div>
+        )}
+
+        {/* Content for tabs */}
+        {activeTab === "tab2" && (
+          <div>
+            <h1 style={{ textAlign: "center" }}>CandyCrush</h1>
+            <Candycrush />
+          </div>
+        )}
+
+        {activeTab === "tab3" && (
+          <div
+            style={{
+              backgroundImage:
+                'url("https://images.pexels.com/photos/12718848/pexels-photo-12718848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              minHeight: "100vh",
+              padding: "20px",
+            }}
+          >
+            <h1 style={{ textAlign: "center" }}>MORE GAMES</h1>
+            <p
+              style={{
+                textAlign: "center",
+                fontStyle: "italic",
+                padding: 20,
+                fontSize: "20px",
+              }}
+            >
+              You can click these links and find more games here:
+            </p>
+            <ul
+              style={{
+                listStyleType: "none",
+                textAlign: "center",
+                padding: 15,
+              }}
+            >
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://www.crazygames.com/game/helix-jump"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "lightblue",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  helix-jump
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://www.crazygames.com/game/tap-tap-shots"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "lightgreen",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  tap-tap-shots
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://www.crazygames.com/game/smash-karts"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "lightcoral",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  smash-karts
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://www.crazygames.com/game/layers-roll"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "lightpink",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  layers-roll
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://www.crazygames.com/game/12-minibattles"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "lightsalmon",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  12-minibattles
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://poki.com/en/g/kubi-pets"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "brown",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  Kubi-Pets
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://poki.com/en/g/cubies"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "purple",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  Cubies
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://poki.com/en/g/sides-of-gravity"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "grey",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  Sides of Gravity
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://poki.com/en/g/crazy-party"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "salmon",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  Crazr Party
+                </a>
+              </li>
+              <li style={{ marginBottom: "10px" }}>
+                <a
+                  href="https://poki.com/en/g/fast-typer"
+                  style={{
+                    textDecoration: "underline",
+                    backgroundColor: "green",
+                    padding: "5px",
+                    fontSize: "30px",
+                  }}
+                >
+                  Fast-Typer
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-
-      {/* Content for tabs */}
-      {activeTab === 'tab1' && (
-        <div>
-          <h1 style={{ textAlign: 'center' }}>TicTacToe</h1>
-          <XO />
-        </div>
-      )}
-
-      {/* Content for tabs */}
-      {activeTab === 'tab2' && (
-        <div>
-          <h1 style={{ textAlign: 'center' }}>CandyCrush</h1>
-          <Candycrush />
-        </div>
-      )}
-
-      {activeTab === 'tab3' && (
-        <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/12718848/pexels-photo-12718848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', padding: '20px' }}>
-          <h1 style={{ textAlign: 'center' }}>MORE GAMES</h1>
-          <p style={{ textAlign: 'center', fontStyle: 'italic', padding: 20, fontSize: '20px' }}>You can click these links and find more games here:</p>
-          <ul style={{ listStyleType: 'none', textAlign: 'center', padding: 15 }}>
-            <li style={{ marginBottom: '10px' }}><a href="https://www.crazygames.com/game/helix-jump" style={{ textDecoration: 'underline', backgroundColor: 'lightblue', padding: '5px', fontSize: '30px'}}>helix-jump</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://www.crazygames.com/game/tap-tap-shots" style={{ textDecoration: 'underline', backgroundColor: 'lightgreen', padding: '5px', fontSize: '30px' }}>tap-tap-shots</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://www.crazygames.com/game/smash-karts" style={{ textDecoration: 'underline', backgroundColor: 'lightcoral', padding: '5px', fontSize: '30px' }}>smash-karts</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://www.crazygames.com/game/layers-roll" style={{ textDecoration: 'underline', backgroundColor: 'lightpink', padding: '5px', fontSize: '30px' }}>layers-roll</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://www.crazygames.com/game/12-minibattles" style={{ textDecoration: 'underline', backgroundColor: 'lightsalmon', padding: '5px', fontSize: '30px' }}>12-minibattles</a></li> 
-            <li style={{ marginBottom: '10px' }}><a href="https://poki.com/en/g/kubi-pets" style={{ textDecoration: 'underline', backgroundColor: 'brown', padding: '5px', fontSize: '30px' }}>Kubi-Pets</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://poki.com/en/g/cubies" style={{ textDecoration: 'underline', backgroundColor: 'purple', padding: '5px', fontSize: '30px' }}>Cubies</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://poki.com/en/g/sides-of-gravity" style={{ textDecoration: 'underline', backgroundColor: 'grey', padding: '5px', fontSize: '30px' }}>Sides of Gravity</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://poki.com/en/g/crazy-party" style={{ textDecoration: 'underline', backgroundColor: 'salmon', padding: '5px', fontSize: '30px' }}>Crazr Party</a></li>
-            <li style={{ marginBottom: '10px' }}><a href="https://poki.com/en/g/fast-typer" style={{ textDecoration: 'underline', backgroundColor: 'green', padding: '5px', fontSize: '30px' }}>Fast-Typer</a></li>
-            
-          </ul>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
--- mindforge-project/src/pages/PostDetails.jsx
+++ mindforge-project/src/pages/PostDetails.jsx
@@ -4,67 +4,82 @@ import { URL, IF } from "../url";
 import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import NavBar from "../components/NavBar";
 
 const PostDetails = () => {
+  const postId = useParams().id;
+  const [post, setPost] = useState({});
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  const postId = useParams().id
-  const [post, setPost] = useState({})
-  const {user} = useContext(UserContext) 
-  const navigate = useNavigate()
-
-  const fetchPosts = async() => {
+  const fetchPosts = async () => {
     try {
-      const res = await axios.get(URL+"/api/posts/"+postId)
-      setPost(res.data)
+      const res = await axios.get(URL + "/api/posts/" + postId);
+      setPost(res.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const handleDelete = async() =>{
+  const handleDelete = async () => {
     try {
-      await axios.delete(URL + "/api/posts/"+postId, {withCredentials:true})
+      await axios.delete(URL + "/api/posts/" + postId, {
+        withCredentials: true,
+      });
       navigate("/blogs");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchPosts()
-  }, [postId])
+    fetchPosts();
+  }, [postId]);
 
   return (
-    <div className="px-8 md:px-[200px] mt-40">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-black md:text-3xl">
-          {post.title}
-        </h1>
-        {user?._id === post?.userId && <div className="flex items-center justify-center space-x-2">
-          <span className="cursor-pointer" onClick={() => navigate("/editpost/"+postId)}>Edit</span>
-          <span className="cursor-pointer" onClick={handleDelete}>Delete</span>
-        </div>}
-        
-      </div>
-      <div className="flex items-center justify-between mt-2 md:mt-4">
-        <p>@{post.username}</p>
-        <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
-      </div>
-      <img src={IF+post.photo} className="w-1/2 relative left-1/4" alt="" />
-      <p>{post.desc}</p>
-      <div className="flex items-center mt-8 space-x-4 font-semibold">
-        <p>Categories:</p>
-        <div className="flex justify-center items-center space-x-2">
-        {post.categories?.map((c, i) =>(
-          <div key={i} className="bg-gray-300 rounded px-3 py-1">{c}</div>
-        ))}
+    <>
+    <NavBar />
+      <div className="px-8 md:px-[200px] mt-40">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-black md:text-3xl">
+            {post.title}
+          </h1>
+          {user?._id === post?.userId && (
+            <div className="flex items-center justify-center space-x-2">
+              <span
+                className="cursor-pointer"
+                onClick={() => navigate("/editpost/" + postId)}
+              >
+                Edit
+              </span>
+              <span className="cursor-pointer" onClick={handleDelete}>
+                Delete
+              </span>
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between mt-2 md:mt-4">
+          <p>@{post.username}</p>
+          <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
+        </div>
+        <img src={IF + post.photo} className="w-1/2 relative left-1/4" alt="" />
+        <p>{post.desc}</p>
+        <div className="flex items-center mt-8 space-x-4 font-semibold">
+          <p>Categories:</p>
+          <div className="flex justify-center items-center space-x-2">
+            {post.categories?.map((c, i) => (
+              <div key={i} className="bg-gray-300 rounded px-3 py-1">
+                {c}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col mt-4">
+          <h3>comments</h3>
         </div>
       </div>
-      <div className="flex flex-col mt-4">
-        <h3>comments</h3>
-      </div>
-    </div>
+    </>
   );
-}
+};
 
-export default PostDetails
+export default PostDetails;
