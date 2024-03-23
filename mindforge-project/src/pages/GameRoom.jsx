8e14af9a58654583745ff61d1818028fa73c4147
Senuth De Silva <senuthdesilva37@gmail.com>
2024-03-23 08:21:17 +0530

    add images and done the more games part

--- mindforge-project/src/pages/GameRoom.jsx
+++ mindforge-project/src/pages/GameRoom.jsx
@@ -89,52 +89,52 @@ const GameItem = ({ name, imageSrc, href }) => (
 const games = [
   {
     name: "helix-jump",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/ee/f7/51/eef751a766111b809b80751eec9d18dd.jpg",
     href: "https://www.crazygames.com/game/helix-jump"
   },
   {
     name: "tap-tap-shots",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/d5/5b/6c/d55b6c725a66bd51dde099652c95cda4.jpg",
     href: "https://www.crazygames.com/game/tap-tap-shots"
   },
   {
     name: "smash-karts",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/15/4a/d1/154ad17f2af0b50432a4dac04e08f273.jpg",
     href: "https://www.crazygames.com/game/smash-karts"
   },
   {
     name: "layers-roll",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/b0/a9/bf/b0a9bfb5e0acf65fa5f3373b3507dcb4.jpg",
     href: "https://www.crazygames.com/game/layers-roll"
   },
   {
     name: "12-minibattles",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/736x/a4/80/47/a480475f6ba13ec9e94ebea6ff4b40cb.jpg",
     href: "https://www.crazygames.com/game/12-minibattles"
   },
   {
     name: "Kubi-Pets",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/95/4c/db/954cdb5d37f9a1cda1f1a961d6f9196a.jpg",
     href: "https://poki.com/en/g/kubi-pets"
   },
   {
     name: "Cubies",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/bd/f2/62/bdf26235be6a2c2a2a2d54b8a498763a.jpg",
     href: "https://poki.com/en/g/cubies"
   },
   {
     name: "Sides of Gravity",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/f2/88/aa/f288aa0e8a64f08a01db2763cc408e1a.jpg",
     href: "https://poki.com/en/g/sides-of-gravity"
   },
   {
     name: "Crazy Party",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/c7/85/96/c7859629d8e77d454c0c9d18034c6972.jpg",
     href: "https://poki.com/en/g/crazy-party"
   },
   {
     name: "Fast-Typer",
-    imageSrc: "https://via.placeholder.com/150",
+    imageSrc: "https://i.pinimg.com/564x/35/fd/9f/35fd9f865e31f99c6055b47d4fb01b72.jpg",
     href: "https://poki.com/en/g/fast-typer"
   }
 ];
