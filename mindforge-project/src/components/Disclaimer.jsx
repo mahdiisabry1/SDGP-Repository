4617585c3258deef683f5f0b7a3cf6e4863e194d
mahdiisabry1 <mahdiisabry@gmail.com>
2025-07-14 22:26:30 +0530

    refactor

--- mindforge-project/src/components/Disclaimer.jsx
+++
@@ -1,9 +0,0 @@
-const Disclaimer = () => {
-  return (
-    <div className="flex justify-center items-center w-full bg-black h-20 text-white hover:bg-blue-500">
-        <p className="uppercase">You must login to create or view posts and Roadmaps</p> 
-    </div>
-  )
-}
-
-export default Disclaimer;
--- mindforge-project/src/url.js
+++
@@ -1,2 +0,0 @@
-export const URL = "mindforge-app.vercel.app"
-export const IF = "mindforge-app.vercel.app/images/";
