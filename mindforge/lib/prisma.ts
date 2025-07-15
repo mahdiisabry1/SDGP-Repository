86f8ec0b67857bb88c30d10e419bef52846d6cc8
mahdiisabry1 <mahdiisabry@gmail.com>
2025-07-15 23:51:39 +0530

    DB
    Configuring the Data base structure and user schema for testing

---
+++ mindforge/lib/prisma.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+// Type definitions
+ declare global {
+   var prisma: PrismaClient | undefined
+ }
+ 
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+export default prisma;
